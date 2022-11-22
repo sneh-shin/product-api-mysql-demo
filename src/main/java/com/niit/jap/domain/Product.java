fcfe06a7228ee80660f38b92fa0c4c51018672eb
sneh-shin <snehashinde800@gmail.com>
2022-11-22 11:16:11 +0530

    added getters and setters, toString() method

--- src/main/java/com/niit/jap/domain/Product.java
+++ src/main/java/com/niit/jap/domain/Product.java
@@ -26,4 +26,46 @@ public class Product {
         this.productDescription = productDescription;
         this.amountInStock = amountInStock;
     }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getAmountInStock() {
+        return amountInStock;
+    }
+
+    public void setAmountInStock(int amountInStock) {
+        this.amountInStock = amountInStock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", amountInStock=" + amountInStock +
+                '}';
+    }
 }
